d202dfa4dbd9d6a13fd7622be8ff3a701c59f00f
Khushal Sarode <khushalsarode25@gmail.com>
2025-06-17 15:19:45 +0530

    Delete Models/model_1 directory

--- Models/model_1/try.py
+++
@@ -1,6 +0,0 @@
-# from ultralytics import YOLO
-
-# Y=YOLO("/content/drive/MyDrive/LOLO/Yolo_Model/runs/LOLOtrain/weights/best.pt")
-# game=Y.predict("/content/daddi.mp4",save=True)
-
-print("hello")
